63eddaa5d647b2e3a2afd2e3f1c24e892d1dcaa8
Kboere <kevin.boere@hva.nl>
2024-12-18 22:27:12 +0100

    small hierarchical change

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -5,7 +5,7 @@ import DarkmodeToggle from "./organisms/darkmode";
 import Logo from "./atoms/logo";
 
 const Header = () => (
-    <><header className="fixed p-4 flex flex-col flex-nowrap w-fit h-[100vh]">
+    <header className="fixed p-4 flex flex-col flex-nowrap w-full h-[100vh] z-10">
     <div className="w-fit">
       <TransitionLink href="/" label="Home">
         <Logo />
@@ -13,10 +13,12 @@ const Header = () => (
     </div>
 
     <Navigation />
-  </header><section className=" absolute right-0 p-4 flex flex-row gap-4">
+
+    <section className=" absolute right-0 p-4 flex flex-row gap-4">
       <Location />
       <DarkmodeToggle />
-    </section></>
+    </section>
+  </header>
 );
 
 export default Header;
