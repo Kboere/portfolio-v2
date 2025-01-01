4ba647dd896dc40efd267b143af96d2dfb8cc0bf
Kboere <kevin.boere@hva.nl>
2025-01-01 21:03:58 +0100

    added option to have a downloadable file

--- src/app/components/molecules/buttons.js
+++ src/app/components/molecules/buttons.js
@@ -1,20 +1,21 @@
-const Buttons = ({ buttons }) => {
+const Buttons = ({ buttons, file = "" }) => {
   return (
     <div className="flex flex-row items-center">
       {buttons.map((item, index) => {
         const { url, target, title } = item.button;
         const buttonStyle = item.button_style || "btn-solid";
 
-        const buttonUrl = item.add_file ? item.add_file : url; 
-        const downloadAttr = item.add_file ? "download" : null; 
+        // Use file path for download if has_file is true; otherwise use URL
+        const buttonUrl = item.has_file ? file : url; 
+        const downloadAttr = item.has_file ? true : null; 
 
         return (
           <a
             key={index}
             href={buttonUrl}
-            target={target || "_self"} 
+            target={target || "_self"}
             className={`btn ${buttonStyle}`}
-            download={downloadAttr}
+            {...(downloadAttr && { download: true })} // Add download attribute if necessary
           >
             {title}
           </a>
--- src/app/components/organisms/about-hero.js
+++ src/app/components/organisms/about-hero.js
@@ -3,35 +3,41 @@ import H1 from "./../atoms/heading/h1";
 import Buttons from "../molecules/buttons";
 
 const AboutHero = ({ aboutData }) => {
-    const skills_sectie = aboutData.skills_sectie;
+  const skills_sectie = aboutData.skills_sectie;
+
+  const filePath = "/CV.pdf";
 
   return (
-      <section className="relative grid md:grid-cols-12 gap-20 md:gap-20 h-screen items-center">
-        <div className="w-full col-span-12 md:col-span-6">
+    <section className="relative grid md:grid-cols-12 gap-20 md:gap-20 h-screen items-center">
+      <div className="w-full col-span-12 md:col-span-6">
         <img
-              className="w-full md:w-auto md:h-full object-contain"
-              src={aboutData.afbeelding.url}
-              alt={aboutData.afbeelding.alt}
-            />
-        </div>
-        <div className="col-span-12 md:col-span-6 flex flex-col gap-4 md:gap-6">
-            <div className="w-full p-6 backdrop-filter backdrop-blur bg-opacity-30 bg-white shadow-md rounded-md flex flex-col gap-4 md:gap-6">
-                {/* slider */}
-                <H1 title={skills_sectie.titel} className="!text-h3 !text-h3m !tracking-normal uppercase"/>
+          className="w-full md:w-auto md:h-full object-contain"
+          src={aboutData.afbeelding.url}
+          alt={aboutData.afbeelding.alt}
+        />
+      </div>
+      <div className="col-span-12 md:col-span-6 flex flex-col gap-4 md:gap-6">
+        <div className="w-full p-6 backdrop-filter backdrop-blur bg-opacity-30 bg-white shadow-md rounded-md flex flex-col gap-4 md:gap-6">
+          {/* Slider */}
+          <H1
+            title={skills_sectie.titel}
+            className="!text-h3 !text-h3m !tracking-normal uppercase"
+          />
 
-                <div dangerouslySetInnerHTML={{
-                  __html: skills_sectie.content,
-                }}/>
-                    
-                <div className="w-full">
-                <Slider slides={skills_sectie.skills_slider} />
-                </div>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: skills_sectie.content,
+            }}
+          />
 
-            </div>
-            <div>
-                <Buttons buttons={aboutData.buttons} />
-            </div>
+          <div className="w-full">
+            <Slider slides={skills_sectie.skills_slider} />
+          </div>
         </div>
+        <div>
+          <Buttons buttons={aboutData.buttons} file={filePath} />
+        </div>
+      </div>
     </section>
   );
 };
