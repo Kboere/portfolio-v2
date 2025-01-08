84cf252842151f1a6a8c0c7e6b9b5e36137b48b6
Kboere <kevin.boere@hva.nl>
2025-01-08 23:22:23 +0100

    ssl changes

--- src/app/config.js
+++ src/app/config.js
@@ -1,5 +1,5 @@
-export const reqUrl = 'http://wordpress.kevinboere.nl/wp-json/wp/v2';
+export const reqUrl = 'https://wordpress.kevinboere.nl/wp-json/wp/v2';
 
-export const reqUrlAcf = 'http://wordpress.kevinboere.nl/wp-json/acf/v3';
+export const reqUrlAcf = 'https://wordpress.kevinboere.nl/wp-json/acf/v3';
 
 // wp-json/acf/v3/options/options
