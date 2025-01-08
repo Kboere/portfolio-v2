45cd2f8c25a926c95cb0e652c30eae2673aff5bb
Kboere <kevin.boere@hva.nl>
2025-01-08 22:33:18 +0100

    mobile changes

--- src/app/components/organisms/PortfolioItemContent.js
+++ src/app/components/organisms/PortfolioItemContent.js
@@ -8,20 +8,31 @@ import ContactForm from "../../components/organisms/contactForm";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const imageStyle = {
-  objectFit: "cover",
-  width: "100%",
-  height: "100%",
-  borderRadius: "20px",
-};
-
 const PortfolioItemContent = ({ post, homeContactData }) => {
   const titleRef = useRef(null);
   const imageRef = useRef(null);
   const sectionRef = useRef(null);
 
+  // make a function were it duplacates the text
+  // const duplicateText = () => {
+  //   const textContainer = document.getElementById("duplicate-text");
+  //   if (textContainer) {
+  //     const textContent = textContainer.textContent; // Get the original text
+  //     textContainer.innerHTML = ""; // Clear the original text
+
+  //     // Create one span and append the text 3 times with periods
+  //     const span = document.createElement("span");
+  //     // Repeat the text 3 times with a period after each
+  //     span.textContent = (textContent + ".").repeat(2).slice(0, -1); // Slice to remove the last period
+  //     textContainer.appendChild(span);
+  //   }
+  // };
+
+
   useEffect(() => {
     if (!titleRef.current || !sectionRef.current) return;
+
+    // duplicateText();
   
     // Scale Effect
     gsap.fromTo(
@@ -75,19 +86,19 @@ const PortfolioItemContent = ({ post, homeContactData }) => {
 
   return (
     <div className="p-8 pb-16 overflow-hidden">
-      <section ref={sectionRef} className="h-itemHero relative flex flex-col items-center justify-end mb-32">
-      <div className="absolute max-w-[1280px] w-[90%]" ref={imageRef}>
+      <section ref={sectionRef} className="h-itemHero relative flex flex-col items-center justify-center md:justify-end mb-32">
+      <div className="absolute max-w-[1280px] w-full md:w-[90%] top-1/2 top-1/2 -translate-y-1/2" ref={imageRef}>
           <Image
             src={post.acf.large_image}
             alt="Portfolio Image"
             width={400}
             height={200}
-            style={imageStyle}
             placeholder="blur"
             blurDataURL="/images/dummy.png"
+            className="rounded-lg md:rounded-[1rem] w-full h-full object-cover"
           />
       </div>
-        <h1 ref={titleRef} className="text-[90px] text-center mb-8">
+        <h1 id="duplicate-text" ref={titleRef} className="text-[32px] md:text-[60px] text-center mt-[66.666%] md:mt-0 md:mb-8 md:whitespace-nowrap">
           {post.title.rendered}
         </h1>
       </section>
